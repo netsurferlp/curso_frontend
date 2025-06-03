bc70fa3d6cb6a97eec31ce106e6c2de7aa9dd786
netsurferlp <gastonpessolano@gmail.com>
2025-06-03 16:31:19 -0300

    php commit

---
+++ pages/envio_mail.php
@@ -0,0 +1,27 @@
+<?php
+
+header("Content-Type: text/html;charset=utf8");
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Correo Recibido</title>
+        
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <main>
+        <div class="container">
+            <h1>Correo Recibido Correctamente!</h1>
+            
+        </div>
+    </main>
+</body>
+</html>
